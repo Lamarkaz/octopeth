0b5f79a9e179bb07a83f618bc23ebc83a1fa34ac
nourharidy <nourharidy@yahoo.com>
2018-06-09 14:03:53 +0200

    Changed Solidity contract

--- contracts/contracts/Octopeth.sol
+++ contracts/contracts/Octopeth.sol
@@ -1,22 +1,32 @@
 pragma solidity ^0.4.21;
 
-contract Octopeth {
+import "./Ownable.sol";
+
+contract Octopeth is Ownable {
 
   struct dApp {
     address owner;
     string title;
     string url;
     string logo;
+    bool approved;
   }
 
+  event Publish(address owner, string title, string url, string logo);
+  event Approve(address owner, string title, string url, string logo);
+
   mapping (string => dApp) dApps;
-  mapping (string => dApp) urls;
 
   function publish(string title, string url, string logo) public {
     require(dApps[title].owner == address(0) || dApps[title].owner == msg.sender);
-    require(urls[url].owner == address(0) || (urls[url].owner == msg.sender));
-    dApp memory newdApp = dApp(msg.sender, title, url, logo);
+    dApp memory newdApp = dApp(msg.sender, title, url, logo, false);
     dApps[title] = newdApp;
-    urls[url] = newdApp;
+    emit Publish(msg.sender, title, url, logo);
+  }
+
+  function approve(string title) public onlyOwner {
+    dApp storage thisdApp = dApps[title];
+    dApps[title].approved = true;
+    emit Approve(msg.sender, thisdApp.title, thisdApp.url, thisdApp.logo);
   }
 }
