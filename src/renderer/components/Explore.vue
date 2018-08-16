d475da23f86816a8854677b0945b8927b3c4ce43
Muhammed <mselrakabawi@gmail.com>
2018-08-16 06:31:29 +0200

    Installation handlers

--- src/renderer/components/Explore.vue
+++ src/renderer/components/Explore.vue
@@ -11,6 +11,7 @@
         <v-flex v-for="i in $store.state.dapps.explore" :key="`3${i.title}`" lg2 style="max-width: 200px; margin-left: 5px; margin-right: 5px" v-if="$store.state.dapps.cat === 'ALL' || translateCat(i.cat) === $store.state.dapps.cat">
           <v-card dark color="secondary">
                 <v-card class="dappCard px-0" style="height: 230px">
+                <v-progress-linear v-if="installingKey == titleDM5(i)" class="installProg" color="purple darken-3" :indeterminate="true" height="4"></v-progress-linear>
                 <v-card-media :src="displayImg(i.logo)" height="130px" class="dappLogo">
                 </v-card-media>
                 <v-card-title primary-title >
@@ -21,7 +22,10 @@
                         <v-icon style="color: white; margin-right: 2px; margin-top: -5px"></v-icon> {{ translateCat(i.cat) }}
                       </v-card-text>
                     </div>
-                    <div class="appBody text-xs-left">{{ i.desc }} <v-btn @click="install(i)">INSTALL</v-btn></div>
+                    <div class="appBody text-xs-left">{{ i.desc }} 
+                      <v-btn v-if="installingKey == titleDM5(i)" class="installBtn" style="margin-left: 0px">INSTALLING...</v-btn>
+                      <v-btn v-else class="installBtn" @click="install(i)">INSTALL</v-btn>
+                    </div>
                   </v-card-text>
                 </v-card-title>
               </v-card>
@@ -34,11 +38,14 @@
 </template>
 
 <script>
+import crypto from 'crypto'
 import path from 'path'
 
 export default {
   data () {
     return {
+      installingKey: '',
+      installText: 'INSTALL'
       // explore: [
       //   {
       //     logo: '',
@@ -71,6 +78,8 @@ export default {
   methods: {
     install: function (dapp) {
       var self = this
+      self.installingKey = crypto.createHash('md5').update(dapp.title).digest('hex')
+      self.installText = 'INSTALLING...'
       var directory = path.join(this.$electron.remote.app.getPath('userData'), '/apps/')
       const simpleGit = require('simple-git')(directory)
       const branchRegex = /(?=tree).*$/ // Non Negative Look-behind Regex for first word after 'tree/'
@@ -81,7 +90,8 @@ export default {
       console.log(directory, repoURL, repoBranch)
       simpleGit.clone(repoURL)
         .then(function () {
-          console.log(dapp.title + 'Dapp Installed Successfully')
+          alert(dapp.title + ' Ðapp Installed Successfully.')
+          self.installing = false
           self.$db.update({
             'data.title': dapp.title
           },
@@ -98,6 +108,7 @@ export default {
         })
         .catch(function (e) {
           self.$electron.remote.dialog.showErrorBox('Error', 'The dApp contents could not be downloaded: ' + e)
+          alert(e)
         })
     },
     displayImg: function (img) {
@@ -126,6 +137,9 @@ export default {
         case '9':
           return 'GOVERNANCE'
       }
+    },
+    titleDM5: function (i) {
+      return crypto.createHash('md5').update(i.title).digest('hex')
     }
   }
 }
@@ -133,107 +147,122 @@ export default {
 
 <style scoped>
 .application--wrap {
-    background-color: #222;
-  }
+  background-color: #222;
+}
 .mainWrapper {
-    margin-right: 90px;
-    margin-left: 90px;
-    min-height: 500px;
-    font-family: 'Dosis', sans-serif;
-    font-weight: 400;
-    font-size: 26px;
-    opacity: 0.8;
-  }
-  .mainWrapperIcon {
-    font-size: 35px !important;
-    margin-right: 5px;
-    color: rgb(107, 32, 172);
-    margin-top: -4px;
-  }
-  .MainWrapperTypo {
-    margin-bottom: 25px;
-    color: rgb(107, 32, 172);
-    font-size: 25px;
-  }
-  .appView {
-    margin-top: 15px;
-    padding-right: 20px;
-    padding-left: 20px;
-    padding-bottom: 20px;
-    width: 100%;
-  }
-  .appTitle {
-    font-weight: 700;
-    font-size: 22px;
-    color: rgb(107, 32, 172);
-    transition: color 800ms ease-in-out;
-  }
-  .dappLogo {
-    padding: 15px;
-    width: 130px;
-    margin-right: auto;
-    margin-left: auto;
-  }
-  .appBody {
-    width: 120%;
-    margin-left: -10px;
-    margin-top: 15px;
-    font-weight: 500;
-    font-size: 15px;
-    position: relative;
-    display: none;
-    transition: all 600ms ease-in-out;
-  }
-  .dappCard {
-    padding-left: 10px;
-    padding-right: 10px;
-    padding-top: 20px;
-    background: rgba(0,0,0,0.4);
-    border-radius: 5px;
-    cursor: pointer;
-    transition: margin-top 600ms ease-in-out;
-  }
-  .categType {
-    width: 100%;
-    height: 30px;
-    left: 0px;
-    bottom: 0px;
-    background: rgba(0,0,0,.3);
-    position: absolute;
-    border-bottom-right-radius: 3px;
-    border-bottom-left-radius: 3px;
-    display: none;
-    transition: none 600ms ease-in-out;
-  }
-  .categText {
-    font-size: 13px;
-    font-weight: 700;
-    color: white;
-    margin-top: -10px;
-  }
-  .dappCard:hover {
-    margin-top: -7px;
-  }
-  .dappCard:hover  .appTitle {
-    color: rgb(107, 32, 172);
-    margin-top: -175px;
-  }
-  .dappCard:hover  .appBody {
-    display: block;
-  }
-  .dappCard:hover  .dappLogo {
-    opacity: 0;
-  }
-  .dappCard:hover  .categType {
-    display: block;
-  }
-  .background {
-    background-color: #222;
-  }
-  .menuItemStyle {
-    color: rgb(107, 32, 172);
-    font-family: 'Dosis', sans-serif;
-    font-size: 18px;
-    font-weight: 500; 
-  }
+  margin-right: 90px;
+  margin-left: 90px;
+  min-height: 500px;
+  font-family: 'Dosis', sans-serif;
+  font-weight: 400;
+  font-size: 26px;
+  opacity: 0.8;
+}
+.mainWrapperIcon {
+  font-size: 35px !important;
+  margin-right: 5px;
+  color: rgb(107, 32, 172);
+  margin-top: -4px;
+}
+.MainWrapperTypo {
+  margin-bottom: 25px;
+  color: rgb(107, 32, 172);
+  font-size: 25px;
+}
+.appView {
+  margin-top: 15px;
+  padding-right: 20px;
+  padding-left: 20px;
+  padding-bottom: 20px;
+  width: 100%;
+}
+.appTitle {
+  font-weight: 700;
+  font-size: 22px;
+  color: rgb(107, 32, 172);
+  transition: color 800ms ease-in-out;
+}
+.dappLogo {
+  padding: 15px;
+  width: 130px;
+  margin-right: auto;
+  margin-left: auto;
+  border-radius: 3px;
+}
+.appBody {
+  width: 120%;
+  margin-left: -10px;
+  margin-top: 15px;
+  font-weight: 500;
+  font-size: 15px;
+  position: relative;
+  display: none;
+  transition: all 600ms ease-in-out;
+}
+.dappCard {
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 20px;
+  background: rgba(0,0,0,0.4);
+  border-radius: 5px;
+  cursor: pointer;
+  transition: margin-top 600ms ease-in-out;
+}
+.categType {
+  width: 100%;
+  height: 30px;
+  left: 0px;
+  bottom: 0px;
+  background: rgba(0,0,0,.3);
+  position: absolute;
+  border-bottom-right-radius: 3px;
+  border-bottom-left-radius: 3px;
+  display: none;
+  transition: none 600ms ease-in-out;
+}
+.categText {
+  font-size: 13px;
+  font-weight: 700;
+  color: white;
+  margin-top: -10px;
+}
+.dappCard:hover {
+  margin-top: -7px;
+}
+.dappCard:hover  .appTitle {
+  color: rgb(107, 32, 172);
+  margin-top: -175px;
+}
+.dappCard:hover  .appBody {
+  display: block;
+}
+.dappCard:hover  .dappLogo {
+  opacity: 0;
+}
+.dappCard:hover  .categType {
+  display: block;
+}
+.background {
+  background-color: #222;
+}
+.menuItemStyle {
+  color: rgb(107, 32, 172);
+  font-family: 'Dosis', sans-serif;
+  font-size: 18px;
+  font-weight: 500; 
+}
+.installBtn {
+  left: 22px;
+  top: 88px;
+  background-color: rgb(107, 32, 172) !important;
+  font-weight: 700;
+  font-size: 13px;
+  position: absolute;
+  border-radius: 9999px;
+  padding: 1px;
+}
+.installProg {
+  margin-top: -20px;
+  box-shadow: none;
+}
 </style>
--- src/renderer/components/Store.vue
+++ src/renderer/components/Store.vue
@@ -8,6 +8,25 @@
     <!-- explore view-->
     <Explore></Explore>
     <!-- <Explore></Explore> -->
+    <v-card>
+      <v-bottom-nav
+        :active.sync="bottomNav"
+        color="purple darken-3"
+        :value="true"
+        fixed
+        class="storeNav"
+      >
+        <v-btn dark>
+          <span>Explore</span>
+          <v-icon>explore</v-icon>
+        </v-btn>
+
+        <v-btn dark>
+          <span>My ÐApps</span>
+          <v-icon>archive</v-icon>
+        </v-btn>
+      </v-bottom-nav>
+    </v-card>
   </div>
 </template>
 
@@ -16,10 +35,23 @@
 export default {
   data () {
     return {
+      bottomNav: 3
+    }
+  },
+  computed: {
+    color () {
+      switch (this.bottomNav) {
+        case 0: return 'purple darken-3'
+        case 1: return 'purple darken-3 '
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.storeNav {
+  max-width: 260px; 
+  
+}
 </style>
